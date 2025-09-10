013e207d2f3d10654d5d5bb3e3a3c07e28dbf858
ex1anhth+quangdv <anhth@trithucmoi.co>
2025-09-10 13:52:57 +0700

    fix site skin and template in initialize

--- config/admin.php
+++ config/admin.php
@@ -295,7 +295,7 @@ return [
     | The orginal adminlte theme is not used anymore.
     |
     */
-    'skin' => 'your-custom-skin-class',
+    'skin' => 'skin-blue-light',
 
     /*
     |--------------------------------------------------------------------------
