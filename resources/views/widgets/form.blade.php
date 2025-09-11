d4cdcaf538263519ff97960ce0dc54ba9c032fc6
ex1anhth+quangdv <anhth@trithucmoi.co>
2025-09-11 10:56:50 +0700

    css ui menu screen

--- resources/views/widgets/form.blade.php
+++ resources/views/widgets/form.blade.php
@@ -15,7 +15,7 @@
         <div class="col-md-{{$width['label']}}"></div>
 
 
-        <div class="col-md-{{$width['field']}} d-flex justify-content-start flex-row-reverse">
+        <div class="col-md-{{$width['field']}}">
 
             @if(in_array('submit', $buttons))
             <div class="btn-group pt-3 pe-2 float-end">
@@ -31,8 +31,8 @@
             @endif
 
             @if(in_array('reset', $buttons))
-            <div class="btn-group pt-3 pe-2">
-                <button type="reset" class="btn btn-warning text-white">{{ trans('admin.reset') }}</button>
+            <div class="btn-group pt-3 pe-2" style="float: left !important;margin-left: 10px;">
+                <button type="reset"class="btn btn-warning text-white">{{ trans('admin.reset') }}</button>
             </div>
             @endif
         </div>
