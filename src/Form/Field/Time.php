08bf07eb6bbd7d99361e9828df32e0e772224091
ex1anhth+quangdv <anhth@trithucmoi.co>
2026-02-27 18:53:26 +0900

    fix: update Time field rendering and clean up unused code

--- src/Form/Field/Time.php
+++ src/Form/Field/Time.php
@@ -2,8 +2,6 @@
 
 namespace OpenAdminCore\Admin\Form\Field;
 
-use OpenAdminCore\Admin\Facades\Admin;
-
 class Time extends Date
 {
     /**
@@ -11,30 +9,38 @@ class Time extends Date
      */
     protected $format = 'HH:mm:ss';
 
+    protected function beforeRender(): void
+    {
+        $selector = $this->getElementClassSelector();
+
+        // Important for HasMany (1-N): this script must be part of the field script stack
+        // so it will be included in the nested templateScript executed after adding a row.
+        $this->script .= <<<JS
+;(function () {
+    var s = '{$selector}';
+    $(document).off('focus.openadmin-time-default', s).on('focus.openadmin-time-default', s, function () {
+        var \$i = $(this);
+        if ((\$i.val() || '').trim()) return;
+        var p = \$i.parent().data('DateTimePicker');
+        if (!p || !window.moment) return;
+        p.date(moment('00:00:00', 'HH:mm:ss'));
+        \$i.val('');
+    });
+})();
+JS;
+    }
+
     /**
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|string
      */
     public function render()
     {
+        // Prevent the picker from auto-filling current time.
+        $this->options['useCurrent'] = $this->options['useCurrent'] ?? false;
+
         $this->prepend('<i class="fa fa-clock-o fa-fw"></i>')
-            ->defaultAttribute('style', 'width: 150px !important; flex: 0 0 auto !important;');
+            ->defaultAttribute('style', 'width:150px !important; flex:0 0 150px;');
 
-        $rendered = parent::render();
-
-        $js = "
-            var picker = $('{$this->getElementClassSelector()}').parent().data('DateTimePicker');
-            var input = $('{$this->getElementClassSelector()}');
-            if (picker) {
-                input.on('focus', function() {
-                    if (!input.val()) {
-                        picker.date(moment('00:00:00', 'HH:mm:ss'));
-                        input.val(''); 
-                    }
-                });
-            }
-            ";  
-        Admin::script($js);
-
-        return $rendered;
+        return parent::render();
     }
 }
